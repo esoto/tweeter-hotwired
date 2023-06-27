c71d0390202057f2cc594730f425548868acfac0
Esteban Soto <esoto074@gmail.com>
2023-06-27 17:04:59 -0600

    Experimenting with chartkick and stimulus callbacks

--- Gemfile
+++ Gemfile
@@ -76,3 +76,5 @@ gem "faker", "~> 3.2"
 gem "pagy", "~> 6.0"
 
 gem "pg_search", "~> 2.3"
+
+gem "chartkick", "~> 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_024143) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_220234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_024143) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.date "date"
+    t.integer "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
